f1d9a351c619a40fd1fdccd8a36150add3685131
Riccardo Battistini <riccardo.battistini2@studio.unibo.it>
2022-03-08 15:37:45 +0100

    Update exercise 6 source file

---
+++ src/u02/Es6.scala
@@ -0,0 +1,14 @@
+def fibonacci(n: Int): Int =
+  @annotation.tailrec
+  def _fib(n: Int, a: Int, b: Int): Int = n match
+    case 0 => a
+    case 1 => b
+    case _ => _fib(n - 1, b, a + b)
+
+  _fib(n, 0, 1)
+
+fibonacci(0)
+fibonacci(1)
+fibonacci(2)
+fibonacci(3)
+fibonacci(4)
