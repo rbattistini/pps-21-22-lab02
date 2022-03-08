46a7c6c380c25fb0ce6f47ec728a1e52d06c0057
Riccardo Battistini <riccardo.battistini2@studio.unibo.it>
2022-03-08 17:19:32 +0100

    Update Es5 type constraints

--- src/main/scala/exercises/Es5.scala
+++ src/main/scala/exercises/Es5.scala
@@ -5,7 +5,7 @@ object Es5 extends App:
   def compose(f: Int => Int, g: Int => Int): Int => Int = (x: Int) => f(g(x))
   
   compose(_ - 1, _ * 2)(5)
-  
-  def composeGeneric[A](f: A => A, g: A => A): A => A = (x: A) => f(g(x))
-  
-  composeGeneric[Double](_ - 1.1, _ * 2.2)(5.1)
+
+  def composeGeneric[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
+
+  composeGeneric[Int, Int, Double](_ - 1.2, _ / 2)(5)
