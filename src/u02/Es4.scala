b9da57a945841a91d7446747b2774603313d825a
Riccardo Battistini <riccardo.battistini2@studio.unibo.it>
2022-03-08 15:37:25 +0100

    Update exercise 4 source file

---
+++ src/u02/Es4.scala
@@ -0,0 +1,17 @@
+val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y <= z
+val part1 = p1(1)(2)
+part1(3)
+part1(1)
+
+val p2 = (x: Int, y: Int, z: Int) => x <= y && y <= z
+p2(1, 2, 3)
+p2(3, 2, 1)
+
+def p3(x: Int)(y: Int )(z: Int): Boolean = x <= y && y <= z
+val part2 = p3(1)(2)
+part2(3)
+part2(1)
+
+def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y <= z
+p4(1, 2, 3)
+p4(3, 2, 1)
